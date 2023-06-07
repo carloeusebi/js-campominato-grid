5b5f5fcf03bd26b8e83a0dafdc218f2187bb7a5d
carloeusebi <carloeusebi@gmail.com>
2023-06-07 17:55:10 +0200

    Completed game

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,14 @@ const difficultyInput = document.getElementById('difficulty');
 const highScoreInput = document.getElementById('high-score');
 const currentScoreInput = document.getElementById('current-score');
 const playButton = document.getElementById('play-button');
+const smile = playButton.querySelector('img')
 
-let bombs = [];
+const mineImage = `<img src="img/mine.png" alt="mine">`;
+const smilePlay = 'img/smile.png';
+const smileOver = 'img/gameover.png';
+
+let cells = [];
+let mines = [];
 let curretnScore;
 let hihgScore;
 
@@ -15,13 +21,10 @@ let hihgScore;
 
 const getNumberOfCells = difficulty => {
     switch (difficulty) {
-
         case 'easy':
             return 100;
-
         case 'hard':
             return 49;
-
         default:
             return 81;
     }
@@ -29,17 +32,16 @@ const getNumberOfCells = difficulty => {
 
 const getRndNumber = max => Math.floor(Math.random() * max) + 1;
 
-const getBombs = max => {
-    const bombs = [];
+const getMines = max => {
+    const mines = [];
 
-    while (bombs.length < 10) {
-        const bomb = getRndNumber(max);
-        if (!bombs.includes(bomb)) {
-            bombs.push(bomb);
+    while (mines.length < 10) {
+        const mine = getRndNumber(max);
+        if (!mines.includes(mine)) {
+            mines.push(mine);
         }
     }
-
-    return bombs;
+    return mines;
 }
 
 const createCell = position => {
@@ -50,13 +52,45 @@ const createCell = position => {
 }
 
 const renderField = (matrix, numberOfCells, difficulty) => {
-
     matrix.className = difficulty;
 
     for (let i = 1; i <= numberOfCells; i++) {
         const cell = createCell(i);
         matrix.appendChild(cell);
+        cell.addEventListener('click', cellClick);
     }
+}
+
+
+const isMine = cell => mines.includes(cell);
+
+const gameOver = thisCell => {
+    thisCell.classList.add('exploded');
+    smile.src = smileOver;
+
+    for (let cell of cells) {
+        cell.removeEventListener('click', cellClick);
+        console.log(cell);
+        const position = parseInt(cell.dataset.position);
+        if (isMine(position)) {
+            cell.innerHTML = mineImage;
+            cell.classList.add('clicked');
+        }
+    }
+}
+
+
+function cellClick() {
+    console.log(this.dataset.position);
+
+    const position = parseInt(this.dataset.position);
+    if (isMine(position)) {
+        gameOver(this);
+        return;
+    }
+
+    this.classList.add('clicked');
+
 }
 
 /*********************************************** */
@@ -67,6 +101,8 @@ const renderField = (matrix, numberOfCells, difficulty) => {
 playButton.addEventListener('click', () => {
     matrix.innerHTML = '';
 
+    smile.src = smilePlay;
+
     const difficulty = difficultyInput.value;
 
     if (difficulty !== 'easy' && difficulty !== 'medium' && difficulty !== 'hard') return;
@@ -75,5 +111,9 @@ playButton.addEventListener('click', () => {
 
     renderField(matrix, numberOfCells, difficulty);
 
-    bombs = getBombs(numberOfCells);
+    mines = getMines(numberOfCells);
+
+    cells = matrix.getElementsByClassName('cells');
+
+    console.log(mines);
 })
