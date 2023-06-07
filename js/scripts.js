24a0d9fbcc64c656d3506206e2984092ae4989c5
carloeusebi <carloeusebi@gmail.com>
2023-06-07 16:36:26 +0200

    Grabbed dom elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('JSOK');
+// DOM Elements
+const matrix = document.getElementById('matrix');
+const difficulty = document.getElementById('difficulty');
+const highScore = document.getElementById('high-score');
+const currentScore = document.getElementById('current-score');
+const playButton = document.getElementById('play-button');
+
+console.log(matrix);
+console.log(difficulty);
+console.log(highScore);
+console.log(currentScore);
+console.log(playButton);
